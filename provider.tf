971effcc58fad003fa4ae907200fa3cc47a14c7a
Pinakin Bhatt <pbbhatt@gmail.com>
2018-11-04 08:51:48 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  #access_key = "AKIAIY7E4AXMHMBWFPAA"
+  access_key = "AKIAJ6YNZJVCQ7RTKQAA"
+  #secret_key = "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"
+  secret_key = "xpzagZZmrK7Exmg5LflHmJvfd7sC4/BHjcWFUi6M"
+  region     = "us-east-1"
+}
